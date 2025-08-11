17596ac2957623c2d028301f9473b6e0cfde25f2
Ville Takanen <vitkukissa@gmail.com>
2025-08-11 22:11:00 +0300

    feat!: mobile bar merged to rail for simplicity
    BREAKING CHANGE: mobile bar styles are no longer available in the design-system

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'BREAKING CHANGE',
+      ],
+    ],
+  },
 };
